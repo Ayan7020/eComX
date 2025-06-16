69400920bbce0779668eb615c41117432c7faa08
Ayan7020 <ayanshaikh.pr@gmail.com>
2025-06-16 17:20:35 +0530

    Feat: Added the u.i of login

--- apps/user-ui/src/app/page.tsx
+++ apps/user-ui/src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Index() {
    * Note: The corresponding styles are in the ./index.tailwind file.
    */
   return (
-    <div>
+    <div className="h-[200vh]">
       Page
     </div>
   );
