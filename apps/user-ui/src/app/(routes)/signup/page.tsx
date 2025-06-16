0c62a0888303b6ec67a10c5d2913e6d77eca24b1
Ayan7020 <ayanshaikh.pr@gmail.com>
2025-06-16 18:35:25 +0530

    feat: added something

---
+++ apps/user-ui/src/app/(routes)/signup/page.tsx
@@ -0,0 +1,163 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import GoogleButton from "../../../shared/components/google-button"
+import { Eye, EyeClosed } from "lucide-react";
+
+type FormData = {
+    email: string;
+    password: string;
+    name: string;
+}
+
+const Page = () => {
+    const [passwordVisible, setpasswordVisible] = useState(false);
+    const [serverError, setserverError] = useState<string | null>(null);
+    const [canResend, setcanResend] = useState(false);
+    const [timer, setTimer] = useState(60);
+    const [showOtp, setshowOtp] = useState(true)
+    const [otp, setotp] = useState(["", "", "", ""])
+    const [userData, setUserData] = useState<FormData | null>(null)
+    const inputRef = useRef<(HTMLInputElement | null)[]>([])
+
+
+    // const router = useRouter();
+
+    const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+
+    const onSubmit = (data: FormData) => {
+
+    };
+
+    const handleOtpChange = (index: number,value: string) => {
+        if(!/^[0-9]?$/.test(value)) return;
+        const newOtp = [...otp];
+        newOtp[index] = value;
+        setotp(newOtp);
+        if(value && index < inputRef.current.length - 1){
+            console.log(inputRef.current.length)
+            inputRef.current[index + 1]?.focus()
+        }
+    }
+
+    const handleOtpKeyDown = (index: number,e: React.KeyboardEvent<HTMLInputElement>) => {
+        if(e.key === "Backspace" && !otp[index] && index > 0) {
+            inputRef.current[index - 1]?.focus()
+        }
+    }
+    return <div className="w-full py-10 min-h-[85vh] bg-[#f1f1f1]">
+        <h1 className="text-4xl font-Poppins font-semibold text-black text-center">
+            Signup
+        </h1>
+        <p className="text-center text-lg font-medium py-3 text-[#00000099]">
+            Home . Signup
+        </p>
+        <div className="w-full flex justify-center">
+            <div className="md:w-[480px] p-8 bg-white shadow rounded-lg ">
+                <h3 className="text-3xl font-semibold text-center mb-2">Login to EComX</h3>
+                <p className="text-center  text-gray-500 mb-4">
+                    Already have an account?
+                    <Link href={"/login"} className="text-blue-500 hover:underline">Login</Link>
+                </p>
+                <GoogleButton />
+                <div className="flex items-center my-5  text-gray-400 text-sm">
+                    <div className="flex-1 border-t border-gray-300" />
+                    <span className="px-3">0r Sign up with Email</span>
+                    <div className="flex-1 border-t border-gray-300" />
+                </div>
+                {!showOtp ? (<form onSubmit={handleSubmit(onSubmit)}>
+                    <label className="block text-gray-700 mb-1">Name</label>
+                    <input
+                        type="text"
+                        placeholder="John"
+                        className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
+                        {...register('name', {
+                            required: "Name is required",
+                        })}
+                    />
+                    {errors.name && (
+                        <p className="text-red-500 text-sm">{String(errors.name.message)}</p>
+                    )}
+                    <label className="block text-gray-700 mb-1">Email</label>
+                    <input
+                        type="email"
+                        placeholder="JohnDoe@gmail.com"
+                        className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
+                        {...register('email', {
+                            required: "Email is required",
+                            pattern: {
+                                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                                message: "Enter a valid email address"
+                            }
+                        })}
+                    />
+                    {errors.email && (
+                        <p className="text-red-500 text-sm">{String(errors.email.message)}</p>
+                    )}
+                    <label className="block text-gray-700 mb-1">Password</label>
+                    <div className="relative">
+                        <input
+                            type={passwordVisible ? "text" : "password"}
+                            placeholder="Min 6 Char"
+                            className="w-full p-2 border border-gray-300 outline-0 !rounded mb-1"
+                            {...register('password', {
+                                required: "Password is required",
+                                minLength: {
+                                    value: 6,
+                                    message: "Password must be at least 6 characters"
+                                }
+                            })}
+                        />
+                        <button type="button" onClick={() => setpasswordVisible(!passwordVisible)} className="absolute inset-y-0 right-3 flex items-center text-gray-400">
+                            {passwordVisible ? <Eye /> : <EyeClosed />}
+                        </button>
+                        {errors.password && (
+                            <p className="text-red-500 text-sm">{String(errors.password.message)}</p>
+                        )}
+                    </div>
+                    <button type="submit" className="w-full text-lg  mt-4 cursor-pointer bg-black text-white py-2 rounded-lg ">Signup
+                    </button>
+                    {serverError && (
+                        <p className="text-red-500 text-sm mt-2">{serverError}</p>
+                    )}
+                </form>) : (
+                    <div>
+                        <h3 className="text-xl font-semibold text-center mb-4">Enter Otp</h3>
+                        <div className="flex justify-center gap-6">
+                            {otp?.map((digit,index) => (
+                                <input type="text"  key={index} ref={(el) => {
+                                    if(el) inputRef.current[index] = el;
+                                }}
+                                maxLength={1}
+                                className="w-12 h-12 text-center border  border-gray-300 outline-none !rounded"
+                                value={digit}
+                                onChange={(e) => handleOtpChange(index,e.target.value)}
+                                onKeyDown={(e) => handleOtpKeyDown(index,e)}
+                                />
+                            ))}
+                        </div>
+                        <button className="w-full text-lg  mt-4 cursor-pointer bg-blue-500 text-white py-2 rounded-lg">
+                            Verify Otp
+                        </button>
+                        <p className="text-center text-sm mt-4">
+                            {
+                                canResend ? (
+                                    <button className="cursor-pointer text-blue-500">
+                                        Resend Otp
+                                    </button>
+                                ): (
+                                    `Resend OTP in ${timer}s`
+                                )
+                            }
+                        </p>
+                    </div>
+                )}
+            </div>
+        </div>
+    </div>
+}
+
+export default Page;
